c7c9bbdc0db476fa122f289575e7a7666b19f56e
OraneOrveillon <71334686+OraneOrveillon@users.noreply.github.com>
2023-08-11 15:06:35 +0200

    First test with quickstart example

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
+import 'data/models/user.dart';
 import 'modules/home/home_binding.dart';
 import 'routes/pages.dart';
 import 'modules/home/home_page.dart';
 import 'core/theme/themes.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // ! *** Base de données *** ! //
+  final dir = await getApplicationDocumentsDirectory();
+  final isar = await Isar.open(
+    [UserSchema],
+    directory: dir.path,
+  );
+
+  final newUser = User()
+    ..name = 'Jane Doe'
+    ..age = 36;
+
+  await isar.writeTxn(() async {
+    await isar.users.put(newUser);
+  });
+
+  // final existingUser = await isar.users.get(newUser.id);
+
+  // await isar.writeTxn(() async {
+  //   await isar.users.delete(existingUser!.id);
+  // });
+  // ! *** *** ! //
+
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
