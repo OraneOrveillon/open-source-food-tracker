0df23ac3b359d44cbb27177d520fc73da1a2db2c
OraneOrveillon <71334686+OraneOrveillon@users.noreply.github.com>
2023-08-25 22:08:41 +0200

    Route names

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,3 +1,5 @@
 const String aliments = "/aliments";
+const String recipes = "/recipes";
 const String journal = "/journal";
+const String statistics = "/statistics";
 const String weighings = "/weighings";
