df62598ca0856336ceccf7bfd1d9a4001697f3c8
OraneOrveillon <71334686+OraneOrveillon@users.noreply.github.com>
2023-09-14 16:17:04 +0200

    Validate button --> AppBar check button

--- lib/modules/recipes/recipe/recipe_page.dart
+++ lib/modules/recipes/recipe/recipe_page.dart
@@ -11,61 +11,58 @@ class RecipePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Recipes'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(Paddings.medium),
-        child: GetBuilder<RecipeController>(
-          builder: (cRecipe) {
-            return Column(
-              children: [
-                Expanded(
-                  child: Form(
-                    key: cRecipe.formKey,
-                    child: ListView(
-                      children: [
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            label: Text(InputTexts.name),
-                          ),
-                          controller: cRecipe.nameTEC,
-                          keyboardType: TextInputType.text,
-                          validator: (value) => Validators.required(value),
-                        ),
-                        // TODO tags
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            label: Text(InputTexts.portions),
-                          ),
-                          controller: cRecipe.portionsTEC,
-                          keyboardType: TextInputType.number,
-                          validator: (value) => Validators.requiredInt(value),
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            label: Text(InputTexts.description),
-                          ),
-                          controller: cRecipe.descriptionTEC,
-                          keyboardType: TextInputType.multiline,
-                          maxLines: null,
-                          validator: (value) => null,
-                        ),
-                        // TODO aliments
-                      ],
+    return GetBuilder<RecipeController>(
+      builder: (cRecipe) {
+        return Scaffold(
+          appBar: AppBar(
+            actions: [
+              IconButton(
+                onPressed: () => cRecipe.onValidateClick(),
+                icon: const Icon(Icons.check),
+              ),
+            ],
+          ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(Paddings.medium),
+              child: Form(
+                key: cRecipe.formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text(InputTexts.name),
+                      ),
+                      controller: cRecipe.nameTEC,
+                      keyboardType: TextInputType.text,
+                      validator: (value) => Validators.required(value),
                     ),
-                  ),
+                    // TODO tags
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text(InputTexts.portions),
+                      ),
+                      controller: cRecipe.portionsTEC,
+                      keyboardType: TextInputType.number,
+                      validator: (value) => Validators.requiredInt(value),
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text(InputTexts.description),
+                      ),
+                      controller: cRecipe.descriptionTEC,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      validator: (value) => null,
+                    ),
+                    // TODO aliments
+                  ],
                 ),
-                ElevatedButton(
-                  onPressed: () => cRecipe.onValidateClick(),
-                  child: const Text(ButtonTexts.validate),
-                )
-              ],
-            );
-          },
-        ),
-      ),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
