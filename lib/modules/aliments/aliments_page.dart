859c02ee075154d2f030b189f6040289756e74bf
OraneOrveillon <71334686+OraneOrveillon@users.noreply.github.com>
2023-11-05 14:44:52 +0100

    Adapt AlimentsPage interface

--- lib/modules/aliments/aliments_page.dart
+++ lib/modules/aliments/aliments_page.dart
@@ -3,13 +3,13 @@ import 'package:get/get.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 import '../../../data/models/aliment_model.dart';
+import '../../core/utils/enums.dart';
 import 'aliments_controller.dart';
 import '../../../core/utils/texts.dart';
 
 class AlimentsPage extends StatelessWidget {
   const AlimentsPage({super.key});
 
-  // TODO Cacher bouton ajout et bouton suppression en mode recipe
   @override
   Widget build(context) {
     return GetBuilder<AlimentsController>(
@@ -27,18 +27,37 @@ class AlimentsPage extends StatelessWidget {
               leading: CircleAvatar(
                 child: Text(aliment.id.toString()),
               ),
-              trailing: IconButton(
-                icon: const Icon(Icons.delete),
-                onPressed: () => cAliments.deleteAliment(aliment),
-              ),
+              trailing: _buildListTileTrailing(cAliments, aliment),
             ),
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () => cAliments.onAddClick(),
-        ),
+        floatingActionButton: _buildFloatingActionButton(cAliments),
       ),
     );
+  }
+
+  Widget? _buildListTileTrailing(
+    AlimentsController cAliments,
+    Aliment aliment,
+  ) {
+    if (cAliments.pageMode == AlimentsPageMode.alimentsModule) {
+      return IconButton(
+        icon: const Icon(Icons.delete),
+        onPressed: () => cAliments.deleteAliment(aliment),
+      );
+    }
+
+    return null;
+  }
+
+  Widget? _buildFloatingActionButton(AlimentsController cAliments) {
+    if (cAliments.pageMode == AlimentsPageMode.alimentsModule) {
+      return FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => cAliments.onAddClick(),
+      );
+    }
+
+    return null;
   }
 }
